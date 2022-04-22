08699eec5f1109258f10dc45ac0e9afb6de40802
kennethhowell <howell.kenneth.a@gmail.com>
2022-04-22 13:58:07 -0500

    complete: refactor to utilize database w/ jpa and dao. navigable thru index to create, index to single page. todo: single page navbar or anchor tag for user to be able to return to index easily

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -18,9 +18,12 @@ public class PostController {
         @GetMapping("/posts")
         public String postsIndex(Model model){
 //                Inside the method that shows all the posts, create a new array list and add two post objects to it, then pass that list to the view.
-                ArrayList<Post> allPosts = new ArrayList<>();
-                allPosts.add(new Post("Good news - I adopted all of the cats", "Come on down to my house and experience northwest San Antonio's first cat cafe called Howlin' Howell's Cat Cafe - WOW!"));
-                allPosts.add(new Post("New special at Howlin' Howell's Cat Cafe!", "We've added a range of new coffee products and TV screens for LAN parties, video game parties, sports events, and otherwise! Call 210-555-5555 to make a reservation today :D"));
+//                ArrayList<Post> allPosts = new ArrayList<>();
+//                allPosts.add(new Post("Good news - I adopted all of the cats", "Come on down to my house and experience northwest San Antonio's first cat cafe called Howlin' Howell's Cat Cafe - WOW!"));
+//                allPosts.add(new Post("New special at Howlin' Howell's Cat Cafe!", "We've added a range of new coffee products and TV screens for LAN parties, video game parties, sports events, and otherwise! Call 210-555-5555 to make a reservation today :D"));
+                //New exercise: Let's move into grabbing our posts from the database instead :)
+
+                ArrayList<Post> allPosts = (ArrayList<Post>) postDao.findAll();
 
                 model.addAttribute("allPosts", allPosts);
 
@@ -29,8 +32,13 @@ public class PostController {
 
         @GetMapping("/posts/{id}")
         public String postView(@PathVariable long id, Model model){
+                //Old exer: hardcoded single post
+//                model.addAttribute("singlePost", new Post("Cats are invading my neighborhood", "Can someone please help me with all of these cats??"));
+                //Now: Let's consume the id with a postDao method. . perhaps getById?
+                Post post = postDao.getById(id);
 
-                model.addAttribute("singlePost", new Post("Cats are invading my neighborhood", "Can someone please help me with all of these cats??"));
+                model.addAttribute("singlePost", post);
+
             return "posts/show";
         }
 

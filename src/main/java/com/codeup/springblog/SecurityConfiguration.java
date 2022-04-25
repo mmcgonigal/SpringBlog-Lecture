015136164bc6e9a37f63b79d28a0ba8a29075023
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-04-25 16:49:07 -0500

    update on class practice

--- src/main/java/com/codeup/springblog/SecurityConfiguration.java
+++ src/main/java/com/codeup/springblog/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.codeup.springblog;
 
 import com.codeup.springblog.services.UserDetailsLoader;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
